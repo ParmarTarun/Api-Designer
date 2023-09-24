a71ab2bfb307cfe5ffd5eb15df96d99f5e428cc6
parmartarun <parmartarun.dev@gmail.com>
2023-09-23 23:45:21 -0400

    :sparkles: Make Responsive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,25 @@
 import { ReactChildrenProps } from "@/types";
 import SideBar from "./SideBar";
+import { RiMenuUnfoldFill } from "react-icons/ri";
+import { useState } from "react";
 
 const Layout = ({ children }: ReactChildrenProps) => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div className="h-screen bg-secondary grid grid-cols-8">
-      <div className="col-span-1 min-h-screen bg-primary">
-        <SideBar />
+    <div className="bg-secondary h-screen">
+      <div className="flex">
+        <SideBar showNav={showNav} setShowNav={setShowNav} />
+        <div className="w-full flex flex-col">
+          <div className="w-full bg-primary text-secondary text-2xl flex items-center lg:hidden md:hidden sm:block">
+            <button onClick={() => setShowNav(!showNav)} className=" p-6 ">
+              <RiMenuUnfoldFill />
+            </button>
+            <h2 className="">API Designer</h2>
+          </div>
+          <div className="p-6">{children}</div>
+        </div>
       </div>
-      <div className=" p-4">{children}</div>
     </div>
   );
 };
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
 import React from "react";
-import { RiSettingsFill, RiUser3Fill, RiDashboardFill } from "react-icons/ri";
-const SideBar = () => {
+import {
+  RiSettingsFill,
+  RiUser3Fill,
+  RiDashboardFill,
+  RiMenuFoldFill,
+} from "react-icons/ri";
+
+interface SideBarProps {
+  showNav: boolean;
+  setShowNav: (p: boolean) => void;
+}
+
+const SideBar = ({ showNav, setShowNav }: SideBarProps) => {
   return (
-    <div className="h-full text-secondary p-10 flex flex-col justify-between">
-      <div className="upper-section">
-        <Link href={"/"} className="logo">
-          <h2 className="text-4xl">API</h2>
-          <h5 className="text-2xl">Designer</h5>
-        </Link>
-        <div className="border-b border-secondary mt-2"></div>
-        <div className="mt-4">
-          <Link href={"/dashboard"} className="text-lg flex items-center">
-            <RiDashboardFill className="mr-2" />
-            Dashboard
+    <aside
+      className={
+        (showNav ? "left-0" : "-left-full") +
+        " h-full text-secondary p-6 flex flex-col justify-between bg-primary fixed w-full md:static md:w-1/5 md:min-h-screen transition-all delay-300"
+      }
+    >
+      <div>
+        <button
+          onClick={() => setShowNav(!showNav)}
+          className="md:hidden absolute right-10 text-2xl"
+        >
+          <RiMenuFoldFill />
+        </button>
+        <div className="upper-section">
+          <Link href={"/"} className="logo">
+            <h2 className="text-4xl">API</h2>
+            <h5 className="text-2xl">Designer</h5>
           </Link>
+          <div className="border-b border-secondary mt-2"></div>
+          <div className="mt-4">
+            <Link href={"/dashboard"} className="text-lg flex items-center">
+              <RiDashboardFill className="mr-2" />
+              Dashboard
+            </Link>
+          </div>
         </div>
       </div>
       <div className="lower-section">
@@ -27,7 +51,7 @@ const SideBar = () => {
           Settings
         </Link>
       </div>
-    </div>
+    </aside>
   );
 };
 
