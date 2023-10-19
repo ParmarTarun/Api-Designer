35f32e089491537ccd737a413a3be259892e95a9
parmartarun <parmartarun.dev@gmail.com>
2023-10-19 18:39:18 -0400

    👽️ Add GET single Collection API

--- lib/collections.ts
+++ lib/collections.ts
@@ -3,8 +3,13 @@ import { mongooseConnect } from "./mongoose";
 import { collectionBody } from "@/types";
 import { InvalidCollectionId } from "./customErrors";
 
-type getCollectionType = () => Promise<collectionType[]>;
-export const getCollections: getCollectionType = async () => {
+type getCollectionsType = () => Promise<collectionType[]>;
+type getCollectionType = (id: string) => Promise<collectionType>;
+type getCollectionByNameType = (name: string) => Promise<collectionType>;
+type postCollectionType = (data: collectionBody) => Promise<collectionType>;
+type deleteCollectionType = (id: string) => Promise<boolean>;
+
+export const getCollections: getCollectionsType = async () => {
   await mongooseConnect();
   const collectionDocs = await Collection.find(
     {},
@@ -25,7 +30,24 @@ export const getCollections: getCollectionType = async () => {
   return collections;
 };
 
-type postCollectionType = (data: collectionBody) => Promise<collectionType>;
+export const getCollection: getCollectionType = async (collId) => {
+  await mongooseConnect();
+  const collection = await Collection.findById(collId);
+  const { name, baseUrl, createdAt, id } = collection as collectionType;
+
+  return { name, baseUrl, createdAt, id };
+};
+
+export const getCollectionByName: getCollectionByNameType = async (
+  collName
+) => {
+  await mongooseConnect();
+  const collection = await Collection.findOne({ name: collName });
+  const { name, baseUrl, createdAt, id } = collection as collectionType;
+
+  return { name, baseUrl, createdAt, id };
+};
+
 export const postCollection: postCollectionType = async ({ name, baseUrl }) => {
   await mongooseConnect();
   const collection = await Collection.create({ name, baseUrl });
@@ -51,7 +73,6 @@ export const patchCollection: patchCollectionType = async (
   return { name, baseUrl, createdAt: collection.createdAt, id: collection._id };
 };
 
-type deleteCollectionType = (id: string) => Promise<boolean>;
 export const deleteCollection: deleteCollectionType = async (id) => {
   await mongooseConnect();
   const result = await Collection.findByIdAndDelete(id);
