80d07c41aebb7a66a1f4b5786ed1a4a5baec9242
parmartarun <parmartarun.dev@gmail.com>
2023-11-30 14:02:33 -0500

    ✨ Add Guest Login

--- components/Home/GetStarted.tsx
+++ components/Home/GetStarted.tsx
@@ -1,10 +1,26 @@
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 const GetStarted = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const handleGetStarted = () => {
     router.push("/auth");
+  };
+  const handleGuestLogin = () => {
+    setIsLoading(true);
+    signIn("credentials", {
+      email: "johndoe@example.com",
+      password: "1234",
+      redirect: false,
+    })
+      .then((res) => {
+        router.push("/collections");
+      })
+      .catch((e) => alert(e))
+      .finally(() => setIsLoading(false));
   };
   return (
     <div className="w-4/5 text-center">
@@ -21,6 +37,18 @@ const GetStarted = () => {
       >
         GET STARTED
       </button>
+      <div>
+        {isLoading ? (
+          <ScaleLoader className="sm:my-10 my-5" />
+        ) : (
+          <button
+            className="bg-primary font-semibold uppercase text-secondary px-4 py-2 rounded-md mt-4"
+            onClick={handleGuestLogin}
+          >
+            Try as a guest
+          </button>
+        )}
+      </div>
     </div>
   );
 };
