8b4926010f9e7cd4d2657a4f8e82bccaa9157016
ParmarTarun <ParmarTarun.dev@gmail.com>
2024-09-17 21:00:46 -0400

    ✨ Add Guest User Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import GetStarted from "@/components/Home/GetStarted";
+import GuestCredsModal from "@/components/Home/GuestCredsModal";
 import Layout from "@/components/Shared/Layout";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <Layout>
       <div className="mt-20 flex flex-col justify-center items-center">
         <GetStarted />
+        <button className="mt-2" onClick={() => setShowModal(true)}>
+          Sign In as guest user?
+        </button>
+        {!!showModal && <GuestCredsModal close={() => setShowModal(false)} />}
       </div>
     </Layout>
   );
